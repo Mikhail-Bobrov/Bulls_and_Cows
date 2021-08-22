d0275e36e13c5ced04696e3c6ad57e7142b5e460
Mikhail <bobrov.mikhail94@gmail.com>
2021-08-22 21:51:12 +0300

    i wanna sleep :D

--- Topics/Processing strings/Double characters/src/Main.java
+++ Topics/Processing strings/Double characters/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String st = scanner.nextLine();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < st.length(); i++) {
+            char c = st.charAt(i);
+
+              sb.append(c).append(c);
+
+
+        }
+        String output = sb.toString();
+        System.out.println(output);
     }
 }
