a584749d2667ab92911c50956db76b4e88b40c58
Mikhail <bobrov.mikhail94@gmail.com>
2021-08-23 19:16:46 +0300

    idk why is dont worked?

--- Topics/Processing strings/Piece of alphabet/src/Main.java
+++ Topics/Processing strings/Piece of alphabet/src/Main.java
@@ -4,6 +4,22 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
+        String letters = "abcdefghijklmnopqrstuvwxyz";
+        boolean result = false;
+        if ((str.length()==2  && letters.contains(str))){
+            result = true;
+        }
+        else if (str.length()==1){
+            result=false;
+        }
 
+        for (int i = 0; i < str.length()-2; i++) {
+            if (letters.contains(str.substring(i,i+3))){
+                result = true;
+            }
+
+
+        }
+        System.out.println(result);
     }
 }
